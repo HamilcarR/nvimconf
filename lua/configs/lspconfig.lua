8aee2fd6fff95422fb80710cab6b0619ce3fbefc
Amine Bensalem <bensalem.amine321@gmail.com>
2025-01-26 14:26:12 +0100

    First commit

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,40 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "clangd" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+lspconfig.clangd.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--compile-commands-dir=" .. vim.fn.getcwd(),
+    "--completion-style=detailed",
+    "--header-insertion=iwyu",
+    "--enable-config",
+    "--background-index",
+    "--pch-storage=memory",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cu" },
+}
+
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
